b74d8097981b03976bf5671aeb55292fc5197b75
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-21 12:35:36 -0700

    4 files to handle the task 0 and 1 requirement(success in 3 tests)

---
+++ main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stddef.h>
+
+/*structures*/
+
+/**
+ * struct format - list of type specifiers/modifying function
+ * @flag: '%' + 'char'
+ * @f: fn ptr specific for each case
+ */
+typedef struct format
+{
+	char *flag;
+	void (*f)(va_list);
+}f_id;
+
+extern f_id format_id[];
+
+/*Functions*/
+
+/*print a char(using write())*/
+int _putchar(char c);
+
+/*funcs for flags*/
+void print_c(va_list args);/*handle char*/
+void print_s(va_list args);/*handle str*/
+void print_percent(va_list args);/*handle '%'*/
+void print_d(va_list args);/*handle dig & int*/
+int format_id_size(void);/*returns size of a f_id element*/
+
+
+/*printf main*/
+int _printf(const char *format, ...);
+#endif /*MAIN_H*/
