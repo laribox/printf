37442d97ed8218cec9b6bf674dcd124e5ae586ca
laribox <laribox2014@gmail.com>
2023-07-22 22:26:55 -0700

    fix the min_int not printing

--- print_numbers.c
+++ print_numbers.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_d - print decimal or integer
@@ -11,9 +12,18 @@ int print_d(va_list args)
 	int num;
 	int div, dig;
 	int chars_printed;
+	char min_int_str[] = "2147483648";
 
 	chars_printed = 0;
 	num = va_arg(args, int);
+	/* Handle the case of INT_MIN (-2147483648) separately */
+	if (num == -2147483648)
+	{
+		chars_printed += _putchar('-');
+		
+		chars_printed += _puts(min_int_str);
+		return (chars_printed);
+	}
 	if (num < 0)
 	{
 		chars_printed += _putchar('-');
