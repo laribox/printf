b058e43f93942a12261f52a601408eaea722be34
laribox <laribox2014@gmail.com>
2023-07-24 04:32:49 -0700

    debugging

--- print_numbers.c
+++ print_numbers.c
@@ -21,25 +21,6 @@ int print_d(va_list args)
 }
 
 /**
- * print_u - print unsigned int
- * @args: passed integer
- * Return: length of printed unsigned int
- */
-int print_u(va_list args)
-{
-	unsigned int num;
-	int chars_printed;
-	char *s;
-
-	chars_printed = 0;
-	num = va_arg(args, unsigned int);
-	s = convert(num, 10, 0);
-
-	_puts(s);
-
-	return (chars_printed);
-}
-/**
  * digit_printer - print input number by digit
  * @num: input number
  * Return: print count
@@ -64,4 +45,35 @@ int digit_printer(int num)
 	chars_printed += _putchar((x % 10) + '0'); /*print 1 dig*/
 
 	return (chars_printed);
+}
+
+/**
+ * print_b - print binary on insigned int
+ * @args: passed integer
+ * Return: length of printed binary
+ */
+int print_b(va_list args)
+{
+	unsigned int num;
+	int i, j;
+	/* Stores binary representation of number.*/
+	int binaryNum[32]; /* Assuming 32 bit integer.*/
+
+	num = va_arg(args, unsigned int);
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	for (i = 0; num > 0; i++)
+	{
+		binaryNum[i] = num % 2;
+		num /= 2;
+	}
+
+	/* Printing array in reverse order.*/
+	for (j = i - 1; j >= 0; j--)
+		_putchar('0' +  binaryNum[j]);
+
+	return (i);
 }
