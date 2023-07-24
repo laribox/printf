d2287077f4223092a6e724ce880cdd5523327471
laribox <laribox2014@gmail.com>
2023-07-24 13:09:26 -0700

    latest

--- _printf.c
+++ _printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
 	va_start(f_args.args, format);
 
 	if (!format || (format[0] == '%' && !format[1]))/*edge cases*/
-	return (-1);
+		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 
