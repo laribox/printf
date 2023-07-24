d56bd90ec13eb1ce6f16935d48862e4f5377f6cd
larini abdellah <laribox2014@gmail.com>
2023-07-24 20:17:37 +0100

    Update main.h

--- main.h
+++ main.h
@@ -7,11 +7,6 @@
 
 /*structures*/
 
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 34814d14b4a8ab9f99561a60ff463c4cc16ba48e
 /**
  * struct arguments - flag precision etc...
  * @args: the args passed for printf
@@ -23,10 +18,6 @@ typedef struct arguments
 	char flags;
 } func_args;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 34814d14b4a8ab9f99561a60ff463c4cc16ba48e
 /**
  * struct format - list of type specifiers/modifying function
  * @c: specifier char
@@ -35,10 +26,6 @@ typedef struct arguments
 typedef struct format
 {
 	char c;
-<<<<<<< HEAD
-=======
-
->>>>>>> 34814d14b4a8ab9f99561a60ff463c4cc16ba48e
 	int (*f)(func_args);
 } s_id;
 
@@ -46,10 +33,7 @@ typedef struct format
 /*Functions*/
 /*Callers*/
 int (*get_fun(char c))(func_args);
-<<<<<<< HEAD
-=======
 
->>>>>>> 34814d14b4a8ab9f99561a60ff463c4cc16ba48e
 
 /*print a char(using write())*/
 int _putchar(char c);
@@ -57,21 +41,14 @@ int _puts(char *s);
 
 /*print funcs per specifiers*/
 /*charachters*/
-<<<<<<< HEAD
-=======
 
->>>>>>> 34814d14b4a8ab9f99561a60ff463c4cc16ba48e
 int print_c(func_args f_args);/*handle char*/
 int print_s(func_args f_args);/*handle str*/
 int print_percent(func_args f_args);/*handle '%'*/
 
 /*number*/
 int print_d(func_args f_args);/*handle dig & int*/
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 34814d14b4a8ab9f99561a60ff463c4cc16ba48e
 int digit_printer(int num);/*solve the int_max issue(print number)*/
 
 
@@ -99,12 +76,6 @@ int print_u(func_args f_args);
 /*special string manipulation*/
 int print_adr(func_args f_args);/*print address*/
 int print_r13(func_args f_args);/*print rot13 of input str*/
-<<<<<<< HEAD
-=======
-
-/*print reversed str*/
-int print_rev(va_list args);
->>>>>>> 34814d14b4a8ab9f99561a60ff463c4cc16ba48e
 
 /*print reversed str*/
 int print_rev(va_list args);
