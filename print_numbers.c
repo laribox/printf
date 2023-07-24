06faa2974b358661d87090b6c8294b3d04ad3067
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-23 18:57:54 -0700

    1

--- print_numbers.c
+++ print_numbers.c
@@ -10,64 +10,58 @@
 int print_d(va_list args)
 {
 	int num;
-	int div, dig;
 	int chars_printed;
 
 	chars_printed = 0;
 	num = va_arg(args, int);
-	/* Handle the case of INT_MIN (-2147483648) separately */
-	if (num == -2147483648)
-	{
-		chars_printed += _puts("-2147483648");
-		return (chars_printed);
-	}
-	if (num < 0)
-	{
-		chars_printed += _putchar('-');
-		num = -num;
-	}
 
-	div = 1;
-	while (num / div > 9)
-		div *= 10;
+	chars_printed += digit_printer(num);
 
-	while (div > 0)
-	{
-		dig = num / div;
-		chars_printed += _putchar('0' + dig);
-		num %= div;
-		div /= 10;
-	}
 	return (chars_printed);
 }
 
 /**
- * print_b - print binary on insigned int
+ * print_u - print unsigned int
  * @args: passed integer
- * Return: length of printed binary
+ * Return: length of printed unsigned int
  */
-int print_b(va_list args)
+int print_u(va_list args)
 {
 	unsigned int num;
-	int i, j;
-	/* Stores binary representation of number.*/
-	int binaryNum[32]; /* Assuming 32 bit integer.*/
+	int chars_printed;
+	char *s;
 
+	chars_printed = 0;
 	num = va_arg(args, unsigned int);
-	if (num == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-	for (i = 0; num > 0; i++)
-	{
-		binaryNum[i] = num % 2;
-		num /= 2;
-	}
+	s = convert(num, 10, 0);
 
-	/* Printing array in reverse order.*/
-	for (j = i - 1; j >= 0; j--)
-		_putchar('0' +  binaryNum[j]);
+	_puts(s);
 
-	return (i);
+	return (chars_printed);
+}
+/**
+ * digit_printer - print input number by digit
+ * @num: input number
+ * Return: print count
+ */
+int digit_printer(int num)
+{
+	unsigned int x;
+	int chars_printed;
+
+	chars_printed = 0;
+	if (num < 0)
+	{
+		chars_printed += _putchar('-');
+		x = -num;
+	}
+	else
+		x = num;
+
+	if (x / 10)
+		digit_printer(x / 10); /*recursion loop*/
+
+	chars_printed += _putchar((x % 10) + '0'); /*print 1 dig*/
+
+	return (chars_printed);
 }
