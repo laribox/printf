15e1ddbe870fadc67091c14f00d02e735ff7166c
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-25 11:06:09 -0700

    teseted %p %S edge cases

--- tests/main.c
+++ tests/main.c
@@ -5,25 +5,28 @@
 
 int main()
 {
-	_printf(" %p\n ", (void*)-1);
-	printf(" %p \n ", (void*)-1);
-	_printf(" %p \n ",  (void*)1);
-	printf(" %p \n ",  (void*)1);
-	_printf(" %p\n  ",  (void*)15);
-	printf(" %p\n  ",  (void*)15);
-	_printf(" %p\n  ",  (void*)16);
-	printf(" %p\n  ",  (void*)16);
-	_printf(" %p\n  ", 17);
-	printf(" %p\n  ", 17);
-	_printf(" %p\n  %p\n  ", LONG_MIN, LONG_MAX);
-	printf(" %p\n  %p\n  ", LONG_MIN, LONG_MAX);
-	_printf(" %p\n  %p\n  ", INT_MIN, INT_MAX);
-	printf(" %p\n %p\n  ", INT_MIN, INT_MAX);
-	_printf(" %p\n %p\n ", ULONG_MAX, -ULONG_MAX);
-	printf(" %p\n %p\n ", ULONG_MAX, -ULONG_MAX);
-	_printf(" %p\n %p\n ", 0, 0);
-	printf(" %p\n %p\n ", 0, 0);
+	_printf("%p\n", (void*)-1);
+	printf("%p\n", (void*)-1);
+	_printf("%p\n",  (void*)1);
+	printf("%p\n",  (void*)1);
+	_printf("%p\n",  (void*)15);
+	printf("%p\n",  (void*)15);
+	_printf("%p\n",  (void*)16);
+	printf("%p\n",  (void*)16);
+	_printf("%p\n", 17);
+	printf("%p\n", 17);
+	_printf("%p\n%p\n", LONG_MIN, LONG_MAX);
+	printf("%p\n%p\n", LONG_MIN, LONG_MAX);
+	_printf("%p\n%p\n", INT_MIN, INT_MAX);
+	printf("%p\n%p\n", INT_MIN, INT_MAX);
+	_printf("%p\n%p\n", ULONG_MAX, -ULONG_MAX);
+	printf("%p\n%p\n", ULONG_MAX, -ULONG_MAX);
+	_printf("%p\n%p\n", 0, 0);
+	printf("%p\n%p\n", 0, 0);
 
 	_printf("%S\n", "Best\nSchool");	
+	_printf("%S\n", "Best\tSchool");	
+	_printf("%S\n", "Best\vSchool");	
+	_printf("%S\n", "Best\bSchool");	
 	return (0);
 }
