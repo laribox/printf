062ce537c29b6deb4f46a487a9e08fb327c0c181
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-25 06:13:14 -0700

    changed places of print_u / print_b

--- print_numbers.c
+++ print_numbers.c
@@ -62,32 +62,35 @@ int digit_printer(int num)
 }
 
 /**
- * print_b - print binary on insigned int
- * @args: passed integer
- * Return: length of printed binary
- */
-int print_b(va_list args)
+* print_u - Print unsigned int
+* @args: passed integer
+* Return: length of printed unsigned int
+*/
+int print_u(va_list args)
 {
 	unsigned int num;
-	int i, j;
-	/* Stores binary representation of number.*/
-	int binaryNum[32]; /* Assuming 32 bit integer.*/
+	unsigned long int div, dig;
+	int chars_printed;
 
+	chars_printed = 0;
 	num = va_arg(args, unsigned int);
+
 	if (num == 0)
 	{
 		_putchar('0');
-		return (1);
+		chars_printed++;
 	}
-	for (i = 0; num > 0; i++)
+
+	div = 1;
+	while (num / div > 9)
+		div *= 10;
+
+	while (div > 0)
 	{
-		binaryNum[i] = num % 2;
-		num /= 2;
+		dig = num / div;
+		chars_printed += _putchar('0' + dig);
+		num %= div;
+		div /= 10;
 	}
-
-	/* Printing array in reverse order.*/
-	for (j = i - 1; j >= 0; j--)
-		_putchar('0' +  binaryNum[j]);
-
-	return (i);
+	return (chars_printed);
 }
