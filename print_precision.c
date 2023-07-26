d3df9036ef5ce4ed29e37d183103d7ddc53e59a9
larini abdellah <laribox2014@gmail.com>
2023-07-26 11:05:41 +0100

    Update print_precision.c

--- print_precision.c
+++ print_precision.c
@@ -6,31 +6,31 @@
  */
 void print_precision(char *precision, int first_arg, types t)
 {
-	int type_digit_count = 0;
+        int type_digit_count = 0;
 
-	if (precision != NULL)
-	{
-		long int num_spaces = 0, i;
-	
-		if (t.integer > 0)
-			type_digit_count = count_digit(t.integer);
-		else			
-			type_digit_count = count_digit(_atoi(t.string));
-		if (precision[0] == '*')
-		{
-			num_spaces = (first_arg < 0) ? 0 : first_arg;
-		}
-		else
-		{
-			num_spaces = _atoi(precision);
-		}
-		for (i = 0; i < num_spaces - type_digit_count; i++)
-		{
-			_putchar(' ');
-		}
-		_putchar(-1);
-	}
-	free(precision);  /*Free allocated memory */
+        if (precision != NULL)
+        {
+                long int num_spaces = 0, i;
+
+                if (t.integer > 0)
+                        type_digit_count = count_digit(t.integer);
+                else
+                        type_digit_count = count_digit(_atoi(t.string));
+                if (precision[0] == '*')
+                {
+                        num_spaces = (first_arg < 0) ? 0 : first_arg;
+                }
+                else
+                {
+                        num_spaces = _atoi(precision);
+                }
+                for (i = 0; i < num_spaces - type_digit_count; i++)
+                {
+                        _putchar(' ');
+                }
+                _putchar(-1);
+        }
+        free(precision);  /*Free allocated memory */
 }
 
 
@@ -41,25 +41,25 @@ void print_precision(char *precision, int first_arg, types t)
  */
 int _atoi(const char *s)
 {
-	int result = 0;
-	int i = 0;
+        int result = 0;
+        int i = 0;
 
-	/* Process each digit in the string */
-	while (s[i] != '\0')
-	{
-		/* Check if the character is a digit */
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			/* Update the result by multiplying by 10 and adding the digit */
-			result = result * 10 + (s[i] - '0');
-		}
-		else
-		{
-			/* Stop processing if a non-digit character is encountered */
-			break;
-		}
-		i++;
-	}
+        /* Process each digit in the string */
+        while (s[i] != '\0')
+        {
+                /* Check if the character is a digit */
+                if (s[i] >= '0' && s[i] <= '9')
+                {
+                        /* Update the result by multiplying by 10 and adding the digit */
+                        result = result * 10 + (s[i] - '0');
+                }
+                else
+                {
+                        /* Stop processing if a non-digit character is encountered */
+                        break;
+                }
+                i++;
+        }
 
-	return (result);
+        return (result);
 }
