806fb0094930c0be5be238d21d3a5c06668ffa9e
laribox <laribox2014@gmail.com>
2023-07-25 19:26:20 -0700

    added get_precision_length

--- get_precision.c
+++ get_precision.c
@@ -7,29 +7,49 @@
  */
 char *get_precision(char *s)
 {
-    if (*s == '*')
-    {
-        char *pre = (char *)malloc(2); // Allocate memory for "*"
-        if (pre != NULL)
-        {
-            pre[0] = '*';
-            pre[1] = '\0';
-        }
-        return pre;
-    }
-    else
-    {
-        int length = 0;
-        while (s[length] >= '0' && s[length] <= '9')
-            length++;
+	if (*s == '*')
+	{
+		char *pre = (char *)malloc(2);
 
-        char *pre = (char *)malloc(length + 1); // Allocate memory for numeric precision
-        if (pre != NULL)
-        {
-            for (int i = 0; i < length; i++)
-                pre[i] = s[i];
-            pre[length] = '\0';
-        }
-        return pre;
-    }
+		if (pre != NULL)
+		{
+			pre[0] = '*';
+			pre[1] = '\0';
+		}
+		return (pre);
+	}
+	else
+	{
+		int length = 0;
+
+		while (s[length] >= '0' && s[length] <= '9')
+			length++;
+
+		char *pre = (char *)malloc(length + 1);
+
+		if (pre != NULL)
+		{
+			for (int i = 0; i < length; i++)
+				pre[i] = s[i];
+			pre[length] = '\0';
+		}
+		return (pre);
+	}
+}
+
+/**
+ * get_precision_length - precision length
+ * @s: passed character after '%'
+ * Return: pointer precision length
+ */
+int get_precision_length(char *s)
+{
+
+	int length = 0;
+
+	while (s[length] >= '0' && s[length] <= '9')
+		length++;
+
+	return (length);
+
 }
