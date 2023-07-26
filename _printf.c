708b60fcab41751d675c876f54b92664f9c2f5a5
larini abdellah <laribox2014@gmail.com>
2023-07-26 11:06:13 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -9,23 +9,23 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int chars_printed;
+        va_list args;
+        int chars_printed;
 
-	va_start(args, format);
-	if (!format || (format[0] == '%' && !format[1])) /*edge cases*/
-	{
-		va_end(args);
-		return (-1);
-	}
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-	{
-		va_end(args);
-		return (-1);
-	}
-	chars_printed = print_helper(format, args);
-	va_end(args);
-	return (chars_printed);
+        va_start(args, format);
+        if (!format || (format[0] == '%' && !format[1])) /*edge cases*/
+        {
+                va_end(args);
+                return (-1);
+        }
+        if (format[0] == '%' && format[1] == ' ' && !format[2])
+        {
+                va_end(args);
+                return (-1);
+        }
+        chars_printed = print_helper(format, args);
+        va_end(args);
+        return (chars_printed);
 }
 /**
  * print_helper - Helper function to process format and print characters
@@ -34,38 +34,38 @@ int _printf(const char *format, ...)
  * Return: number of printed chars
  */
 int print_helper(const char *format, va_list args)
-{	int chars_printed = 0;
-	const char *p;
-	char *pre;
-	int (*p_fun)(va_list, flag *, length_mod *, char *, char *);
-	flag f = {0, 0, 0};
-	length_mod len;
+{       int chars_printed = 0;
+        const char *p;
+        char *pre;
+        int (*p_fun)(va_list, flag *, length_mod *, char *, char *);
+        flag f = {0, 0, 0};
+        length_mod len;
 
-	for (p = format; *p != '\0'; p++) /*loop input str*/
-	{	len.h = 0;
-		len.l = 0;
-		if (*p == '%') /*specifier check*/
-		{	p++;
-			while (get_flags(*p, &f)) /*check for flag and increment*/
-				p++;
-			pre = get_precision(p, &p);
-			while (get_len_mod(*p, &len)) /*check for flag and increment*/
-				p++;
-			p_fun = get_fun(*p); /*print_fun call*/
-			if (p_fun)
-			{	chars_printed += p_fun(args, &f, &len, pre, "");
-			}
-			else /*test correct outputs later*/
-			{	_putchar('%');
-				_putchar(*p);
-				chars_printed = chars_printed + 2;
-			}
-		}
-		else
-		{	_putchar(*p);
-			chars_printed++;
-		}
-	}
-	_putchar(-1);
-	return (chars_printed);
+        for (p = format; *p != '\0'; p++) /*loop input str*/
+        {       len.h = 0;
+                len.l = 0;
+                if (*p == '%') /*specifier check*/
+                {       p++;
+                        while (get_flags(*p, &f)) /*check for flag and increment*/
+                                p++;
+                        pre = get_precision(p, &p);
+                        while (get_len_mod(*p, &len)) /*check for flag and increment*/
+                                p++;
+                        p_fun = get_fun(*p); /*print_fun call*/
+                        if (p_fun)
+                        {       chars_printed += p_fun(args, &f, &len, pre, "");
+                        }
+                        else /*test correct outputs later*/
+                        {       _putchar('%');
+                                _putchar(*p);
+                                chars_printed = chars_printed + 2;
+                        }
+                }
+                else
+                {       _putchar(*p);
+                        chars_printed++;
+                }
+        }
+        _putchar(-1);
+        return (chars_printed);
 }
